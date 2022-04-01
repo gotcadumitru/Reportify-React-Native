6cbe4e5171cfd43a0d72dd349f5887784000fc9b
Dumitru Gotca <dum.gotca@gmail.com>
2022-04-01 15:46:17 +0300

    Splash screen init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+import {Text} from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
+
+export default function App() {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      // await RNBootSplash.hide({fade: true});
+      console.log('Bootsplash has been hidden successfully');
+    });
+  }, []);
+
+  return <Text>My awesome app</Text>;
+}
