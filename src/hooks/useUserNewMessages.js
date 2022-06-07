34bf70d12e363af3a4e814afba3c7cd486e158a2
Dumitru Gotca <dum.gotca@gmail.com>
2022-06-07 16:33:22 +0300

    Update user message

--- src/hooks/useUserNewMessages.js
+++ src/hooks/useUserNewMessages.js
@@ -2,41 +2,53 @@ import {useCallback, useEffect, useRef} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {io} from 'socket.io-client';
 import {getAllUserMessages} from 'app-redux/actions/app/app.actions';
+import axios from 'axios';
 const useUserNewMessages = () => {
   const user = useSelector(state => state.appReducer.profile);
   const socket = useRef();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllUserMessages(user._id));
-    socket.current = io('http://192.168.43.252:8080/');
+    socket.current = io('http://192.168.149.22:8080/');
 
-    socket.current.on('getMessage', () => {
+    socket.current.on('newMessage', () => {
       dispatch(getAllUserMessages(user._id));
     });
     // eslint-disable-next-line
   }, []);
 
   useEffect(() => {
-    socket.current.emit('addUser', user._id);
+    socket.current.emit('addUser', {idForUser: user._id, idForModerator: null});
     return () => {
       socket.current.disconnect();
     };
     // eslint-disable-next-line
   }, [user.id]);
 
-  const sendMessage = useCallback(
-    (text, receiverId) => {
-      const message = {
-        senderId: user._id,
-        text: text,
-        receiverId,
-        moderatorId: null,
-      };
-      socket.current.emit('sendMessage', message);
-    },
-    // eslint-disable-next-line
-    [user.id],
-  );
+  // const sendMessage = useCallback(
+  //   (text, receiverId) => {
+  //     const message = {
+  //       senderId: user._id,
+  //       text: text,
+  //       receiverId,
+  //       moderatorId: null,
+  //     };
+  //     socket.current.emit('sendMessage', message);
+  //   },
+  //   // eslint-disable-next-line
+  //   [user.id],
+
+  const sendMessage = async (text, receiverId) => {
+    const message = {
+      senderId: user._id,
+      text: text,
+      receiverId,
+      moderatorId: null,
+    };
+    try {
+      await axios.put(`/api/message/send-message`, message);
+    } catch (error) {}
+  };
 
   return sendMessage;
 };
